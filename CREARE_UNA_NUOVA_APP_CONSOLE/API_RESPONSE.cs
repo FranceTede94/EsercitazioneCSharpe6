e252ac0ea329cfab7b25609fad4bae4ceb8b8d7f
FranceTede94 <francesco.tedesco94@hotlook.com>
2025-03-25 23:18:26 +0100

    Esercitazione C#

---
+++ CREARE_UNA_NUOVA_APP_CONSOLE/API_RESPONSE.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CREARE_UNA_NUOVA_APP_CONSOLE
+{
+    internal class API_RESPONSE
+    {
+        private int _userId;
+        private int _id;
+        private string _title;
+        private string _body;
+
+        public API_RESPONSE(int userId, int id, string title, string body)
+        {
+            UserId = userId;
+            Id = id;
+            Title = title;
+            Body = body;
+        }
+
+        public int UserId { get => _userId; set => _userId = value; }
+        public int Id { get => _id; set => _id = value; }
+        public string Title { get => _title; set => _title = value; }
+        public string Body { get => _body; set => _body = value; }
+    }
+}
---
+++ CREARE_UNA_NUOVA_APP_CONSOLE/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+
+namespace CREARE_UNA_NUOVA_APP_CONSOLE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // Creiamo un client per fare richieste web
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    // Facciamo una richiesta GET per ottenere i dati da un sito
+                    var response = client.GetAsync("https://jsonplaceholder.typicode.com/posts").Result;
+
+                    // Leggiamo i dati che ci sono arrivati dalla risposta
+                    var responseStream = response.Content.ReadAsStreamAsync().Result;
+
+                    // Trasformiamo i dati JSON in una lista di oggetti
+                    var responseData = JsonSerializer.DeserializeAsync<List<API_RESPONSE>>(responseStream).Result;
+
+                    // Stampiamo le informazioni di ogni post
+                    foreach (var post in responseData)
+                    {
+                        Console.WriteLine("User ID: " + post.UserId); // Mostra l'ID dell'utente
+                        Console.WriteLine("ID: " + post.Id); // Mostra l'ID del post
+                        Console.WriteLine("Title: " + post.Title); // Mostra il titolo del post
+                        Console.WriteLine("Body: " + post.Body); // Mostra il corpo del post
+                        Console.WriteLine("-------------------"); // Linea separatrice tra i post
+                    }
+                }
+                catch (Exception ex) // Se c'è un errore, lo catturiamo
+                {
+                    // Mostra il messaggio di errore
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
